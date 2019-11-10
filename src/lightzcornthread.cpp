90e36710a64be2a3b213a509617b97f2b66467cf
Pro X <pro@proteanx.com>
2019-11-10 17:55:14 -0500

    const qualifier for catch exceptions

--- src/lightzcornthread.cpp
+++ src/lightzcornthread.cpp
@@ -59,7 +59,7 @@ void CLightWorker::ThreadLightZCORNSimplified() {
                                 heightStop
                         );
 
-                    } catch (NotEnoughMintsException e) {
+                    } catch (const NotEnoughMintsException e) {
                         LogPrintStr(std::string("ThreadLightZCORNSimplified: ") + e.message + "\n");
                         rejectWork(genWit, blockHeight, NOT_ENOUGH_MINTS);
                         continue;
@@ -92,7 +92,7 @@ void CLightWorker::ThreadLightZCORNSimplified() {
                     rejectWork(genWit, blockHeight, NON_DETERMINED);
                 }
             }
-        } catch (std::exception& e) {
+        } catch (const std::exception& e) {
             //std::cout << "exception in light loop, closing it. " << e.what() << std::endl;
             PrintExceptionContinue(&e, "lightzcornthread");
             break;
